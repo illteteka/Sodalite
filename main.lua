8cd033996ff9df7a9eb7b225a83ada1ff6824259
illteteka <sick.gilmartin@gmail.com>
2020-06-22 19:24:14 -0400

    Looking at changes

--- main.lua
+++ main.lua
@@ -23,6 +23,7 @@ ctrl_name = "ctrl"
 ctrl_id = "Ctrl"
 ctrl_cursor = "b"
 mac_string = false
+slash_dir = "/"
 
 a_key = _OFF
 lctrl_key = _OFF
@@ -403,6 +404,10 @@ function love.load()
 		ctrl_id = "Cmd"
 		ctrl_cursor = "w"
 		mac_string = true
+	end
+	
+	if love.system.getOS() == "Windows" then
+		slash_dir = "\\"
 	end
 
 	math.randomseed(os.time())
